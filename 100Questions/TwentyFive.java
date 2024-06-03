6d9855d299721f27f281ea19c6190ac9e08eabf4
Ajay Pawar <157605873+pawarajay2207@users.noreply.github.com>
2024-06-03 20:09:04 +0530

    Add files via upload
    Number Triangle Pattern

---
+++ 100Questions/TwentyFive.java
@@ -0,0 +1,26 @@
+public class TwentyFive {
+
+	public static void printPattern(int n)
+	{
+		int i, j;
+		
+		for (i = 1; i <= n; i++) {
+			
+			for (j = 1; j <= n - i; j++) {
+				System.out.print(" ");
+			}
+		
+			for (j = 1; j <= i; j++) {
+				System.out.print(i + " ");
+			}
+			
+			System.out.println();
+		}
+	}
+public static void main(String args[])
+	{
+		int n = 6;
+		printPattern(n);
+	}
+}
+//Write a java program to print a Number triangle pattern
